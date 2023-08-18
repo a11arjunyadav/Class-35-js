a4ab95ba4342a06260250150d02ab6b7d80a9299
a11arjunyadav <yadavarjun0312@gmail.com>
2023-08-18 18:25:16 +0530

    js

---
+++ custom3.js
@@ -0,0 +1,5 @@
+//console.log('hello2');
+
+console.log('India\'s'+" Inducting "+"Yasin "+' Malik\'s'+" wife"+` Mushaal`+' Hussein'+" Mullick");
+
+//console.log("Hello \"OKLABS Students\""); //using Escap charector
---
+++ custom4.js
@@ -0,0 +1,8 @@
+
+// + as conectination operators
+console.log('Anil'+ 20); //string + number
+console.log("Arjun"+ 30); //string + number
+console.log (`Raja`+ 40); //string + number
+console.log(50+'Abhisek');
+console.log(40+30); //nuber + number addition operator
+console.log("30"+"40"); //string + concatination operator
